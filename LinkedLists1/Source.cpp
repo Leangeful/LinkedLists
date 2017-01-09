6d300fd47daf9f14f5da5c0f963f49e3f6bebb4d
Leangeful <cuttheleash1@gmail.com>
2017-01-09 12:16:35 +0100

    ...

---
+++ LinkedLists1/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct node {
+	int data;
+	node *next;
+};
+
+node* insertFront(node *head, int info) {
+	node *temp;
+	temp = (node*)malloc(sizeof(node));
+	temp->data = info;
+	temp->next = head;
+	return temp;
+}
+
+void traverse(node *head) {
+	node *temp1 = head;
+
+	while (temp1 != NULL) {
+		cout << temp1->data << " ";
+		temp1 = temp1->next;
+	}
+}
+
+int main() {
+	node *head = NULL;
+
+	int T = 5;
+
+	while (T--) {
+		int input;
+		cin >> input;
+		head = insertFront(head, input);
+	}
+
+	traverse(head);
+
+
+
+
+	return 0;
+}
